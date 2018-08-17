69ecf35d8d14392e0c49c4e02fe70b5785d7ce08
lailakh <lailakhas@gmail.com>
2018-08-18 00:06:02 +0300

    search form

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,13 @@ $('li > strong > a').click(function() {
     $('li > strong > a').removeClass('active');
     $(this).addClass('active');
 });
+
+$(document).ready(function(){
+  $('.search-form').on("click",(function(e){
+  $(".form-group").addClass("sb-search-open");
+    e.stopPropagation()
+  }));
+
+
+ 
+})
